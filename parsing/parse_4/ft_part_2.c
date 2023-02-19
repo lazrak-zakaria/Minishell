fa4919e409a78656ffa6b80d0ea3419fb98c0570
zakaria lazrak <z.zakarialazrak@gmail.com>
2023-02-19 10:40:21 +0100

    p

---
+++ parsing/parse_4/ft_part_2.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_part_2.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zlazrak <zlazrak@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/18 11:00:21 by zlazrak           #+#    #+#             */
+/*   Updated: 2023/02/18 17:19:27 by zlazrak          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	ft_norm_p_2(char *a, t_var *var)
+{
+	while (a[var->i])
+	{
+		if (a[var->i] == '|')
+		{
+			if (var->flag)
+			{
+				ft_push(&var->queue_answer, ft_new_node(var->vec.string));
+				memset(&var->vec, 0, sizeof(var->vec));
+				var->flag = 0;
+			}
+			ft_push(&var->queue_answer,
+				ft_new_node((ft_substr(a, var->i, var->i + 1))));
+			var->i++;
+		}
+		else
+			ft_norm_p1_2(a, var);
+	}
+}
+
+t_queue	*ft_part_2(t_queue *queue)
+{
+	t_var	var;
+	char	*a;
+
+	var.queue_answer = NULL;
+	while (queue)
+	{
+		memset(&var.vec, 0, sizeof(t_vector));
+		var.flag = 0;
+		var.i = 0;
+		var.temp_queue = ft_pop(&queue);
+		a = var.temp_queue->data;
+		ft_norm_p_2(a, &var);
+		free (var.temp_queue);
+		free (a);
+	}
+	return (var.queue_answer);
+}
