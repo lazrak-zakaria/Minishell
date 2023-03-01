580b327e0c5fea7d27c7a86189c822b6a201e84f
zakaria lazrak <z.zakarialazrak@gmail.com>
2023-03-01 13:21:40 +0100

    norm

--- queue.h
+++ queue.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   queue.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yel-mass <yel-mass@student.42.fr>          +#+  +:+       +#+        */
+/*   By: zlazrak <zlazrak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 12:59:04 by yel-mass          #+#    #+#             */
-/*   Updated: 2023/02/27 13:00:00 by yel-mass         ###   ########.fr       */
+/*   Updated: 2023/03/01 13:20:04 by zlazrak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,13 @@
 # include <string.h>
 # include <stdlib.h>
 
-typedef void* TYPE;
-
 typedef struct s_queue
 {
 	void			*data;
 	struct s_queue	*next;
 }	t_queue;
 
-t_queue	*ft_new_node(TYPE data);
+t_queue	*ft_new_node(void *data);
 void	ft_push(t_queue **top, t_queue *q);
 t_queue	*ft_pop(t_queue **top);
 int		ft_q_size(t_queue *top);
