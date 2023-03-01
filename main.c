adcbb04f138f571e0217bc57123b5eff4a7816a6
zakaria lazrak <z.zakarialazrak@gmail.com>
2023-03-01 12:02:04 +0100

    norm

--- main.c
+++ main.c
@@ -6,27 +6,26 @@
 /*   By: zlazrak <zlazrak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 13:02:54 by yel-mass          #+#    #+#             */
-/*   Updated: 2023/03/01 11:47:38 by zlazrak          ###   ########.fr       */
+/*   Updated: 2023/03/01 11:59:20 by zlazrak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_prompt	g_prompt;
-
-void	ft_signal_handler(int sg)
+t_prompt 	prompt;
+void    ft_signal_handler(int sg)
 {
 	if (sg == SIGINT)
 	{
-		if (g_prompt.flag == 2)
+		if (prompt.flag == 2)
 		{
 			ft_putchar_fd('\n', 1);
 			return ;
 		}
-		if (g_prompt.flag == 1)
+		if (prompt.flag == 1)
 			return ;
 		else
-			g_prompt.exit_status = 1;
+			prompt.exit_status = 1;
 		ft_putchar_fd('\n', 1);
 		rl_replace_line("", 0);
 		rl_on_new_line();
@@ -34,55 +33,55 @@ void	ft_signal_handler(int sg)
 	}
 }
 
-/*
+void    ft_signal(void)
+{
 	// struct termios    mini_shell; 
 
 	// tcgetattr(STDIN_FILENO, &mini_shell); 
 	// mini_shell.c_lflag &= ~ECHOCTL; 
 	// tcsetattr(STDIN_FILENO, TCSANOW, &mini_shell);
-*/
 
-void	ft_signal(void)
-{
 	signal(SIGINT, ft_signal_handler);
 	signal(SIGQUIT, SIG_IGN);
-}
-
-void	ft_minishell(void)
-{
-	char	*a;
-
-	while ("*_*")
-	{
-		g_prompt.list_cmd = NULL;
-		a = readline("minishell: ");
-		if (a == NULL)
-		{
-			printf("exit\n");
-			exit(g_prompt.exit_status);
-		}
-		if (a[0])
-			add_history(a);
-		if (ft_parse(a, &g_prompt))
-		{
-			free(a);
-			continue ;
-		}
-		free(a);
-		ft_exec(&g_prompt);
-		ft_free_lis(g_prompt.list_cmd);
-	}
 }
 
 int	main(int ac, char **av, char **env)
 {
 	(void)ac;
 	(void)av;
-	g_prompt.flag = 0;
+	
+
+	//ex_status = &prompt.exit_status;
+	prompt.flag = 0;
 	ft_signal();
-	g_prompt.s_env = ft_creat_env(env);
-	g_prompt.env = get_env(g_prompt.s_env);
-	g_prompt.exit_status = 0;
-	g_prompt.list_cmd = NULL;
-	ft_minishell();
+	prompt.s_env = ft_creat_env(env); //		linked list
+	prompt.env = get_env(prompt.s_env); // 		char ** alocated
+	//prompt.exit_status = 0;
+	prompt.list_cmd = NULL;
+	while(1)
+	{
+		//printf("%d_", prompt.exit_status);
+		prompt.list_cmd = NULL;
+		char *a = readline("minishell: ");
+		if (a == NULL)
+		{
+			printf("exit\n");
+			exit(prompt.exit_status);
+		}
+		if(a[0])
+			add_history(a);
+		if (ft_parse(a, &prompt))
+		{
+			free(a);
+			continue ;
+		}
+		free(a);
+
+	/******************************/
+		t_list *oo = prompt.list_cmd;
+		ft_exec(&prompt);
+		ft_free_lis(oo);
+
+	}
 }
+ 
